c4f81df0b343155f7ddf8c21651aa02d3aef87b5
Pushpak Jalan <pushpakjalan07@outlook.com>
2020-01-08 14:46:09 +0530

    Correctly Implemented Search-Pivot
    Correctly implemented search_pivot function in Rotated Array problem.

--- Search_Element_In_Sorted_And_Rotated_Array.py
+++ Search_Element_In_Sorted_And_Rotated_Array.py
@@ -2,16 +2,27 @@
 
 import sys
 
-def binary_search():
+#def binary_search():
 
-def search_pivot(list_of_nos, start, end, nos):
-    if()
-    mid = start + ((end - start)/2)
-    if(list_of_nos[mid] > list_of_nos[mid+1] && list_of_nos[mid] > list_of_nos[mid-1]):
-        return mid
+def search_pivot(list_of_nos, start, end):
+    if(end - start + 1 == 1):
+        return start
+    elif(end - start + 1 == 2):
+        if(list_of_nos[start] < list_of_nos[end]):
+            return start
+        else:
+            return end
     else:
-        pivot = 
-        return pivot
+        mid = int(start + ((end - start)/2))
+        if(list_of_nos[mid] < list_of_nos[mid+1] and list_of_nos[mid] < list_of_nos[mid-1]):
+            return mid
+        else:
+            if(list_of_nos[mid] < list_of_nos[start]):                                          # search left
+                return search_pivot(list_of_nos, start, mid-1)
+            elif(list_of_nos[mid] > list_of_nos[end]):                                          # search right
+                return search_pivot(list_of_nos, mid+1, end)
+            else:
+                return 0
 
 def main():
     nos = int(input("Enter No. of Nos.: "))
@@ -19,7 +30,7 @@ def main():
     if(len(list_of_nos) != nos):
         print("Invalid Input\n\n")
         sys.exit()
-    search_pivot(list_of_nos, 0, nos - 1)
+    print(search_pivot(list_of_nos, 0, nos - 1))
     return
 
 if __name__ == "__main__":
