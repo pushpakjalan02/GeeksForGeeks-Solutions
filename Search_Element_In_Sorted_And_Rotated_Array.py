ee081ab7f34ec52eacc6070d47e246ada19651e5
Pushpak Jalan <pushpakjalan07@outlook.com>
2020-01-08 15:42:12 +0530

    Completed Search Element in Rotated Array
    Completed Search Element in Rotated Array Successfully.

--- Search_Element_In_Sorted_And_Rotated_Array.py
+++ Search_Element_In_Sorted_And_Rotated_Array.py
@@ -2,7 +2,28 @@
 
 import sys
 
-#def binary_search():
+def binary_search(list_of_nos, start, end, search_val):
+    if(start <= end):
+        mid = int(start + ((end - start)/2))
+        if(search_val == list_of_nos[mid]):
+            return mid
+        elif(search_val > list_of_nos[mid]):
+            return binary_search(list_of_nos, mid+1, end, search_val)
+        else:
+            return binary_search(list_of_nos, start, mid-1, search_val)
+    else:
+        return -1
+
+def search(list_of_nos, start, end, pivot):
+    search_val = int(input("Enter Search Value: "))
+    if(list_of_nos[start] <= search_val):
+        if(pivot == 0):
+            return binary_search(list_of_nos, start, end, search_val)
+        else:
+            return binary_search(list_of_nos, start, pivot-1, search_val)
+    else:
+        return binary_search(list_of_nos, pivot, end, search_val)
+        
 
 def search_pivot(list_of_nos, start, end):
     if(end - start + 1 == 1):
@@ -30,7 +51,13 @@ def main():
     if(len(list_of_nos) != nos):
         print("Invalid Input\n\n")
         sys.exit()
-    print(search_pivot(list_of_nos, 0, nos - 1))
+    pivot = search_pivot(list_of_nos, 0, nos - 1)
+    ret_val = search(list_of_nos, 0, nos - 1, pivot)
+    if(ret_val != -1):
+        print("Found at Index: ", ret_val)
+    else:
+        print("Not Found")
+        
     return
 
 if __name__ == "__main__":
