3b94fc742ccefe5afff8043f644f5fc0e461b10c
Pushpak Jalan <pushpakjalan07@outlook.com>
2020-01-12 00:26:01 +0530

    Question and Links Added

---
+++ Max_Sum_of_Element_Indices.py
@@ -0,0 +1,3 @@
+# Find maximum value of Sum( i*arr[i]) with only rotations on given array allowed
+# https://www.geeksforgeeks.org/find-maximum-value-of-sum-iarri-with-only-rotations-on-given-array-allowed/
+
