3021972d42cc451c9886e9e91604ee92b9fecf65
Pushpak Jalan <pushpakjalan07@outlook.com>
2020-01-21 23:43:38 +0530

    Create Rotation_With_Maximum_Hamming_Distance.py

---
+++ Rotation_With_Maximum_Hamming_Distance.py
@@ -0,0 +1,4 @@
+# Find a rotation with maximum hamming distance
+# Given an array of n elements, create a new array which is a rotation of given array and hamming distance between both the arrays is maximum.
+
+# https://www.geeksforgeeks.org/find-a-rotation-with-maximum-hamming-distance/
