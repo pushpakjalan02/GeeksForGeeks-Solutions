bf56d1deaf35d0e90c177e4a138fca960067a554
Pushpak Jalan <pushpakjalan07@outlook.com>
2020-01-22 00:04:47 +0530

    Update Rotation_With_Maximum_Hamming_Distance.py

--- Rotation_With_Maximum_Hamming_Distance.py
+++ Rotation_With_Maximum_Hamming_Distance.py
@@ -6,15 +6,24 @@
 import sys
 
 def max_hamming_distance(list_of_nos, nos):
-    
-    return
+    max = -1
+    sum = 0
+    for i in range(0, nos):
+        for j in range(0, nos):
+            if(list_of_nos[j] != list_of_nos[(i + j) % nos]):
+                sum += 1
+        if(max < sum):
+            max = sum
+        sum = 0
+    return max
 
 def main():
     nos = int(input("Enter no. of nos.: "))
     list_of_nos = list(map(int, input("Enter List of Nos.").strip().split()))
     if(len(list_of_nos) != nos):
         sys.exit(0)
-    max_hamming_distance(list_of_nos, nos)
+    max_val = max_hamming_distance(list_of_nos, nos)
+    print("Maximum Hamming Distance: ", max_val)
     return
 
 if __name__ == "__main__":
