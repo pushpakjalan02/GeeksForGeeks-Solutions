32e713261961189e693f804b67adc804f8f888b9
Pushpak Jalan <pushpakjalan07@outlook.com>
2020-01-22 23:53:18 +0530

    Create Possible_to_Sort_after_Rotation.py

---
+++ Possible_to_Sort_after_Rotation.py
@@ -0,0 +1,19 @@
+# Check if it is possible to sort the array after rotating it
+
+# URL: https://www.geeksforgeeks.org/check-if-it-is-possible-to-sort-the-array-after-rotating-it/
+
+def find_answer(list_of_nos, nos):
+    
+    return
+
+def main():
+    nos = int(input("Enter No. of Nos."))
+    list_of_nos = list(int, input("Enter the list of Nos.").strip().split())
+    if(len(list_of_nos) != nos):
+        return
+    answer = find_answer(list_of_nos, nos)
+    print(answer)
+    return
+
+if __name__ == "__main__":
+    main()
