b58e6239eb49bc80f6bf2b3193e1fc4b7594944d
Pushpak Jalan <pushpakjalan07@outlook.com>
2020-01-22 23:37:49 +0530

    Update Rotate_Bits_of_a_Number.py

--- Rotate_Bits_of_a_Number.py
+++ Rotate_Bits_of_a_Number.py
@@ -1,3 +1,16 @@
 # Rotate bits of a number
 
 # URL: https://www.geeksforgeeks.org/rotate-bits-of-an-integer/
+
+import sys
+
+def main():
+    no = int(input("Enter No."))
+    rotate_by = int(input("Rotate by: "))
+    print("After Left Rotate: ", ((no << rotate_by) + (no >> (32 - rotate_by))) & 0xffffffff)
+    print("After Right Rotate: ", ((no << (32 - rotate_by)) + (no >> rotate_by)) & 0xffffffff)
+
+    return
+
+if __name__ == "__main__":
+    main()
